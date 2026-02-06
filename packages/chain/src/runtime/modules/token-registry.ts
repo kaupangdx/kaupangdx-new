d211fc888fcd73d4e3e0158b22136659f6420014
saitunc <aytunc.sancar@dynastystudios.io>
2026-02-06 17:58:48 +0300

    refactor: wraped TokenRegistry to runtimeModule

--- packages/chain/src/runtime/modules/token-registry.ts
+++ packages/chain/src/runtime/modules/token-registry.ts
@@ -1,6 +1,6 @@
 import { NoConfig } from "@proto-kit/common";
 import { TokenId } from "@proto-kit/library";
-import { RuntimeModule } from "@proto-kit/module";
+import { runtimeMethod, runtimeModule, RuntimeModule } from "@proto-kit/module";
 import { State, StateMap, state } from "@proto-kit/protocol";
 import { Field } from "o1js";
 
@@ -12,10 +12,12 @@ export const MAX_TOKEN_ID = 99999;
 /**
  * Maintains an incremental registry of all the token IDs in circulation.
  */
+@runtimeModule()
 export class TokenRegistry extends RuntimeModule<NoConfig> {
   @state() tokenIds = StateMap.from<TokenIdId, TokenId>(TokenIdId, TokenId);
   @state() lastTokenIdId = State.from(TokenIdId);
 
+  @runtimeMethod()
   public async addTokenId(tokenId: TokenId) {
     const lastTokenIdId = await this.lastTokenIdId.get();
     const nextTokenIdId = lastTokenIdId.value.add(1);
